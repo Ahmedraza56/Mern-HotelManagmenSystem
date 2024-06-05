34803e43c902fbc3affe88390d64073ee8be38e7
ahmed <ahmedrazy830@gmail.com>
2024-06-05 11:34:56 +0500

    initial commit

--- server/index.js
+++ server/index.js
@@ -20,11 +20,7 @@ app.use('/uploads', express.static('uploads'));
 // CORS configuration
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:3000',
-    'http://localhost:5176',
-    'http://localhost:5175'
+    'https://hms-hodx69dgv-raza69s-projects.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
